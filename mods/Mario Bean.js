85aea7b90f3bee970f5e5cf9f083b1d733f6d640
Furkannk <140885352+Furkannk@users.noreply.github.com>
2024-01-21 16:22:15 +0300

    Mario's beans

---
+++ mods/Mario Bean.js
@@ -0,0 +1,9 @@
+elements.delicious_Bean = {
+	color: "#563F37",
+	behavior: behaviors.LIQUID,
+	category: "food",
+	state: "fluid",
+	reactions: {
+		"delicious_Bean": { elem1: "bean", elem2: "oil" },
+	}
+};
